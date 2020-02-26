d5a03498e53f8e97e6d0c31cb540e229ce3181d1
Paul Savignano <Paul.Savignano@gmail.com>
2020-02-25 19:52:27 -0800

    Updates

---
+++ precache-manifest.f08998b305e71d9c770368f3260c7e82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b08cc0a30abeee78b2d97821148e6da",
+    "url": "/window-splitter/index.html"
+  },
+  {
+    "revision": "bb5e4f476c4c80106b89",
+    "url": "/window-splitter/static/css/main.349704d3.chunk.css"
+  },
+  {
+    "revision": "9538a6e61a14c6c27ec1",
+    "url": "/window-splitter/static/js/2.ec6bb7ec.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/window-splitter/static/js/2.ec6bb7ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb5e4f476c4c80106b89",
+    "url": "/window-splitter/static/js/main.6124b07f.chunk.js"
+  },
+  {
+    "revision": "4daba85e73381e75984f",
+    "url": "/window-splitter/static/js/runtime-main.c8a689c2.js"
+  }
+]);
